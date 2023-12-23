15d15e2e40e746896a4238f8e8b644e19d6d38df
Seweryn <sew_k@wp.pl>
2023-12-23 14:44:49 +0100

    Added implementation of methods in Wall class.

--- src/main/java/org/horus/recruitmenttask/applicationlogic/Wall.java
+++ src/main/java/org/horus/recruitmenttask/applicationlogic/Wall.java
@@ -1,21 +1,39 @@
 package org.horus.recruitmenttask.applicationlogic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class Wall implements Structure {
+public class Wall implements Structure{
     private List<Block> blocks;
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return null;
+        return getBlocks().stream()
+                .filter(b -> b.getMaterial().equals(material))
+                .collect(Collectors.toList());
     }
     @Override
     public Optional<Block> findBlocksByColor(String color) {
-        return null;
+        return getBlocks().stream()
+                .filter(b -> b.getColor().equals(color))
+                .findAny();
     }
     @Override
     public int count() {
-        return -1;
+        return getBlocks().size();
+    }
+    public List<Block> getBlocks() {
+        return blocks;
+    }
+    public Wall() {
+        this.blocks = new ArrayList<>();
+    }
+    public void buildByCompositeBlock(CompositeBlock compositeBlock) {
+        this.blocks.addAll(compositeBlock.getBlocks());
+    }
+    public void buildByBlock(Block block) {
+        this.blocks.add(block);
     }
 }
